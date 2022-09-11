6158d3a8114cd814188178c865550529f692aa08
Leonardo Cezar <leocezar@gmail.com>
2022-09-11 18:07:51 -0300

    Adicionado 01---TurmaEmpiricus27062022

---
+++ 01 - TurmaEmpiricus27062022/src/br/com/empiricus/exercicios27062022/HelloWorld.java
@@ -0,0 +1,11 @@
+package br.com.empiricus.exercicios27062022;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		System.out.println("Meu primeiro código!!");
+		System.out.println("Leonardo || Lorena || Liliane");
+	}
+
+}
